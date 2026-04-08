50b775e8d3eabadf25dce2b43f4f491560ba196e
RadekPlawecki <radoslaw.a.plawecki@gmail.com>
2026-04-08 07:23:11 +0200

    added the Prim algorithm

--- algorithms.py
+++ algorithms.py
@@ -4,6 +4,7 @@
 
 from union_find import UnionFind
 from graph_utils import draw_graph_with_mst
+import heapq
 
 
 def kruskal(n, edges):
@@ -15,4 +16,28 @@ def kruskal(n, edges):
         if uf.union(u, v):  
             mst.append((u, v, w))
             total_weight += w
+    return mst, total_weight
+
+
+def prim(n, edges):
+    graph = [[] for _ in range(n)]
+    for u, v, w in edges:
+        graph[u].append((v, w))
+        graph[v].append((u, w))
+
+    visited = [False] * n
+    mst = []
+    total_weight = 0
+    heap = [(0, 0, -1)]  
+    while heap and len(mst) < n - 1:
+        w, u, parent = heapq.heappop(heap)
+        if visited[u]:
+            continue
+        visited[u] = True
+        if parent != -1:
+            mst.append((parent, u, w))
+            total_weight += w
+        for v, weight in graph[u]:
+            if not visited[v]:
+                heapq.heappush(heap, (weight, v, u))
     return mst, total_weight
